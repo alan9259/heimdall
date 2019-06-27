b4db07928b81e30b59f383b414a82642aa03a2a0
Alan Wang <Steve9259@hotmail.com>
2019-06-27 22:37:07 +1200

    move model to main for testing purpose

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -8,6 +8,21 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Account struct {
+	ID           string    `json:"id,omitempty"`
+	EmailAddress string    `json:"email_address,omitempty"`
+	FirstName    string    `json:"first_name,omitempty"`
+	LastName     string    `json:"last_name,omitempty"`
+	Location     *Location `json:"location,omitempty"`
+}
+
+type Location struct {
+	City      string `json:"city,omitempty"`
+	State     string `json:"state,omitempty"`
+	Country   string `json:"country,omitempty"`
+	IpAddress string `json:"ip_address,omitempty"`
+}
+
 func Login(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode("i am a fake token")
 }
