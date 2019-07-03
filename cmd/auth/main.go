e78b8451bc50531b43a81bd367c18ca8b327e35f
Alan Wang <Steve9259@hotmail.com>
2019-07-03 20:37:48 +1200

    utilise internal custom package

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -3,25 +3,11 @@ package main
 import (
 	"encoding/json"
 	"log"
+	model "miu-auth-api-v1/internal/models"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
-
-type Account struct {
-	ID           string    `json:"id,omitempty"`
-	EmailAddress string    `json:"email_address,omitempty"`
-	FirstName    string    `json:"first_name,omitempty"`
-	LastName     string    `json:"last_name,omitempty"`
-	Location     *Location `json:"location,omitempty"`
-}
-
-type Location struct {
-	City      string `json:"city,omitempty"`
-	State     string `json:"state,omitempty"`
-	Country   string `json:"country,omitempty"`
-	IpAddress string `json:"ip_address,omitempty"`
-}
 
 func Login(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode("i am a fake token")
@@ -32,7 +18,7 @@ func Logout(w http.ResponseWriter, req *http.Request) {
 }
 
 func Register(w http.ResponseWriter, req *http.Request) {
-	var account Account
+	var account model.Account
 	_ = json.NewDecoder(req.Body).Decode(&account)
 
 	json.NewEncoder(w).Encode(account)
