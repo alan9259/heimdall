deac7726e3a5647389ab6e45e8f099e54c1dac16
Jason Wu <zwu047@hotmail.com>
2019-07-11 15:28:17 +1200

    config store handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,17 +2,20 @@ package handler
 
 import (
 	account "miu-auth-api-v1/internal/account"
+	config "miu-auth-api-v1/internal/config"
 	location "miu-auth-api-v1/internal/location"
 )
 
 type Handler struct {
 	accountStore  account.Store
 	locationStore location.Store
+	configStore   config.Store
 }
 
-func NewHandler(as account.Store, ls location.Store) *Handler {
+func NewHandler(as account.Store, ls location.Store, cs config.Store) *Handler {
 	return &Handler{
 		accountStore:  as,
 		locationStore: ls,
+		configStore:   cs,
 	}
 }
