2146cdb2fc62c0cb854bb7831ec1bed7e5d0dad0
Kenneth D'souza <kennyspeaks@gmail.com>
2019-07-16 21:42:53 +1200

    Modified Generate and Verify Pin functions

--- internal/handler/pin.go
+++ internal/handler/pin.go
@@ -30,24 +30,18 @@ type pinValidateResponseModel struct {
 type pinGenerateRequestModel struct {
 	emailAddress string
 	pin          int
-	expiredAt    time.Time
-	purpose      string
 }
 
 type pinGenerateResponseModel struct {
-	emailAddress string
-	pin          int
-	expiredAt    time.Time
-	purpose      string
+	pin       int
+	expiredAt time.Time
 }
 
 func generatePin(a *pinModel) *pinGenerateResponseModel {
 	resp := new(pinGenerateResponseModel)
 	t := time.Now()
 	resp.pin = rand.Intn(10000)
-	resp.emailAddress = "kennyspeaks@gmail.com"
 	resp.expiredAt = t.AddDate(0, 0, 3)
-	resp.purpose = "VerifyEmail"
 	return resp
 }
 
