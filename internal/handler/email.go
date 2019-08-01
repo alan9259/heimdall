e29ac2ea57c7cd81247544579b0d903c4a4be20b
Grogan, Declan <Declan.Grogan@ccc.govt.nz>
2019-08-01 20:09:57 +1200

    padZeros function

--- internal/handler/email.go
+++ internal/handler/email.go
@@ -11,12 +11,13 @@ func (h *Handler) sendVerifyEmail(a *model.Account, p *model.Pin) error {
 	senderEmail := "alan9259@gmail.com"
 	subject := "Thank you for signing up"
 	content := ""
+	strPin := strconv.Itoa(int(p.Pin))
 	if p.Purpose == "verify" {
 		content = "Hi " + a.FirstName + ", <br><br>" + "Thank you for signing up, " +
-			"please verify your email address by entering the verification code: " + strconv.Itoa(int(p.Pin)) + " in your app." + "<br><br>" + "MIU"
+			"please verify your email address by entering the verification code: " + padZeros(strPin) + " in your app." + "<br><br>" + "MIU"
 	} else {
 		content = "Hi " + a.FirstName + ", <br><br>" + "You've forgotten your password! " +
-			"You will find the reset password screen after entering the verification code: " + strconv.Itoa(int(p.Pin)) + " in your app." + "<br><br>" + "MIU"
+			"You will find the reset password screen after entering the verification code: " + padZeros(strPin) + " in your app." + "<br><br>" + "MIU"
 	}
 
 	err := h.emailService.SendEmail(
@@ -33,4 +34,13 @@ func (h *Handler) sendVerifyEmail(a *model.Account, p *model.Pin) error {
 	}
 
 	return nil
+}
+
+func padZeros(pin string) (result string) {
+	var pinStr = pin
+	if len(pinStr) == 6 {
+		return pinStr
+	}
+	pinStr = "0" + pinStr
+	return padZeros(pinStr)
 }
