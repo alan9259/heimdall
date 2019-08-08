5cc69d53606aed28a10ed16d22b13698dd3d4c90
Alan Wang <Steve9259@hotmail.com>
2019-08-09 09:37:45 +1200

    added unit test TestCurrentAccountCaseInvalid and TestUpdateAccountMultipleFields

--- internal/handler/account_test.go
+++ internal/handler/account_test.go
@@ -82,3 +82,57 @@ func TestGetCurrentAccountSuccess(t *testing.T) {
 		assert.Equal(t, "alan@miu.com", m["email_address"])
 	}
 }
+
+func TestCurrentAccountCaseInvalid(t *testing.T) {
+	tearDown()
+	setup()
+	jwtMiddleware := middleware.JWT(platform.JWTSecret, h.revokedTokenStore)
+	req := httptest.NewRequest(echo.GET, "/api/accounts/login", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	req.Header.Set(echo.HeaderAuthorization, authHeader(platform.GenerateJWTToken(100)))
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	err := jwtMiddleware(func(context echo.Context) error {
+		return h.GetCurrentAccount(c)
+	})(c)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusNotFound, rec.Code)
+}
+
+func TestUpdateAccountMultipleFields(t *testing.T) {
+	tearDown()
+	setup()
+	var (
+		account1UpdateReq = `{
+			"first_name":"alan", 
+			"last_name": "wang", 
+			"gender_id":2, 
+			"phone_number": 111, 
+			"date_of_birth": "1985-04-28T10:00:00Z"
+		}`
+	)
+	jwtMiddleware := middleware.JWT(platform.JWTSecret, h.revokedTokenStore)
+	req := httptest.NewRequest(echo.PUT, "/api/profiles", strings.NewReader(account1UpdateReq))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	req.Header.Set(echo.HeaderAuthorization, authHeader(platform.GenerateJWTToken(1)))
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	err := jwtMiddleware(func(context echo.Context) error {
+		return h.UpdateAccount(c)
+	})(c)
+	assert.NoError(t, err)
+
+	err = jwtMiddleware(func(context echo.Context) error {
+		return h.GetCurrentAccount(c)
+	})(c)
+
+	assert.NoError(t, err)
+
+	if assert.Equal(t, http.StatusOK, rec.Code) {
+		m := responseMap(rec.Body.Bytes())
+		assert.Equal(t, "alan", m["first_name"])
+		assert.Equal(t, "wang", m["last_name"])
+		assert.Equal(t, "male", m["gender"])
+		assert.NotEmpty(t, m["token"])
+	}
+}
